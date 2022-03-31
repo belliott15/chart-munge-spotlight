cff2cf3c75a69c1c03517c3af9dcc7d33bb6cdf2
Beau Elliott <belliott1516@gmail.com>
2022-03-31 16:50:54 -0700

    created charts and began writing function for bar chart

--- app.js
+++ app.js
@@ -1,8 +1,76 @@
-// import functions and grab DOM elements
 
-// let state
+  // Bar chart showing number of customers by each purchase frequency.
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+};
+
+const myBarChart = new Chart(
+    document.getElementById('barChart'),
+    config
+);
+
+  // Line chart showing number of customers by each cool factor.
+const lineData = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+const lineConfig = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const myLineChart = new Chart(
+    document.getElementById('lineChart'),
+    lineConfig
+);
+  // Pie chart showing number of customers by each gender. 
+    // Make sure you have enough colors in your chart data to make 
+    // the pie chart readable.
+const pieData = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(215, 90, 131)',
+        borderColor: 'rgb(200, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+const pieConfig = {
+    type: 'pie',
+    data: data,
+    options: {}
+};
+  
+const mypieChart = new Chart(
+    document.getElementById('pieChart'),
+    pieConfig
+);
+
